5759e16ff10e8b79f088179c2cc35c61f5a0a76b
Taras Kozodoi <acateely@gmail.com>
2022-11-07 20:37:27 +0200

    first commit

---
+++ src/main/java/com/rscar/rscar/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.rscar.rscar;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "Hello word" + "</h1>");
+        out.println("</body></html>");
+    }
+}
